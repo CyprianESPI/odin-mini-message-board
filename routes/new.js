e806b4247e0b81197fcff0ec6015283ebf55e684
CyprianESPI <cyprianespinoza@gmail.com>
2025-01-03 15:58:11 +0100

    Add form

--- routes/new.js
+++ routes/new.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('new'));
+router.get('/', (req, res) => res.render('form'));
 
 module.exports = router;
