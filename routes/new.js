a9583608c50938137725786d7cc77f0d337c569e
CyprianESPI <cyprianespinoza@gmail.com>
2025-01-03 16:14:42 +0100

    Add basic navigation

--- routes/new.js
+++ routes/new.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', (req, res) => res.render('form'));
 router.post('/', (req, res) => {
     const body = req.body;
-    const message = {text: body.message, user: body.author, date: new Date() };
+    const message = {text: body.message, user: body.author, added: new Date() };
     messages.push(message);
     res.render('form');
 });
