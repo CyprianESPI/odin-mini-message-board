25427b6c026153683961910647040baf06c9127b
CyprianESPI <cyprianespinoza@gmail.com>
2025-01-03 15:26:38 +0100

    Add basic app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const path = require("node:path");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const HOST_NAME = "localhost";
+
+// Enable EJS as the view engine, will look for templates in /views dir
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+    console.log(`Listening on port http://${HOST_NAME}:${PORT}`);
+});
