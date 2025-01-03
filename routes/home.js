c0e967adacb710fe12d6101fcaeda0af9323b57a
CyprianESPI <cyprianespinoza@gmail.com>
2025-01-03 15:40:14 +0100

    Use index.ejs for / view

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,20 @@
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+const TITLE = "Mini Messageboard";
+
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('ok'));
+router.get('/', (req, res) => res.render("index", {title: TITLE}));
 
 module.exports = router;
