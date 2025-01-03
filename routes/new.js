24627628712c01eb7db6a745a74b116d41a94fc4
CyprianESPI <cyprianespinoza@gmail.com>
2025-01-03 16:11:32 +0100

    Add messages from user

--- routes/new.js
+++ routes/new.js
@@ -1,6 +1,13 @@
+const messages = require("../models/db");
 const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => res.render('form'));
+router.post('/', (req, res) => {
+    const body = req.body;
+    const message = {text: body.message, user: body.author, date: new Date() };
+    messages.push(message);
+    res.render('form');
+});
 
 module.exports = router;
